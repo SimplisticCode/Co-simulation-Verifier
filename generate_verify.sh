13c5064ee097f0ea1d0fe626bd6cc6e32aaaa7e3
Simon Thrane Hansen <sth@eng.au.dk>
2020-12-15 09:47:24 +0100

    Added logging to scripts

--- generate_verify.sh
+++ generate_verify.sh
@@ -10,12 +10,12 @@ mkdir Uppall-models/examples
 for f in Scenario/examples/*.conf
 do 
     OUT=./Uppall-models/examples/$(basename ${f%.*}).xml
-    java -D"log4j.configurationFile=./log4j2.xml" -jar $JARFILE -m ${f} -o $OUT 
+    java -D"log4j.configurationFile=./log4j2.xml"  -jar $JARFILE -m ${f} -o $OUT --verify 
 done
 
 mkdir Uppall-models/should_fail
 for f in Scenario/common_mistakes/*.conf
 do 
     OUT=./Uppall-models/should_fail/$(basename ${f%.*}).xml
-    java -D"log4j.configurationFile=./log4j2.xml" -jar $JARFILE -m ${f} -o $OUT 
+    java -D"log4j.configurationFile=./log4j2.xml" -jar $JARFILE -m ${f} -o $OUT
 done
